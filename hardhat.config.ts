06b75a6eed130100ccc8c0aa3590f7eec86cf0dc
Maksym H <mordamax@gmail.com>
2025-08-27 17:40:23 +0100

    fix

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,8 +11,10 @@ dotenv.config();
 const config: HardhatUserConfig = {
   solidity: "0.8.30",
   resolc: {
-    compilerSource: "npm",
-    version: "0.3.0"
+    compilerSource: "binary",
+    settings: {
+      resolcPath: './bin/resolc',
+    }
   },
   networks: {
     hardhat: {
